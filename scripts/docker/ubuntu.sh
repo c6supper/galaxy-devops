c8c61ba5537c6f27c12a419919bfafe466d258b4
c6supper <c6supper@hotmail.com>
2020-04-21 03:28:26 +0000

    get the latest docker

--- scripts/docker/ubuntu.sh
+++ scripts/docker/ubuntu.sh
@@ -8,3 +8,13 @@ usermod -aG docker c6supper
 curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+
+rm -rf /etc/docker/daemon.json
+touch /etc/docker/daemon.json
+cat >> /etc/docker/daemon.json << EOF
+{
+    "registry-mirrors": ["https://registry.docker-cn.com"]
+} 
+EOF
+
+service docker restart
