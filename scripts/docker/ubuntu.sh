70e1315a1456b3dbb670eb418f739edbd8d6a3de
c6supper <c6supper@hotmail.com>
2020-04-21 03:18:37 +0000

    get the latest docker

--- scripts/docker/ubuntu.sh
+++ scripts/docker/ubuntu.sh
@@ -1,4 +1,10 @@
 
 #!/bin/sh
 
-apt-get install -y docker.io
+apt-get remove docker docker-engine docker.io docker-ce docker-ce-cli
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+usermod -aG docker c6supper
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
