e11b818ec7944772d5c9e79753148e4b12a6943b
c6supper <c6supper@hotmail.com>
2020-05-06 09:42:37 +0000

    update for promotion sudo

--- scripts/docker/ubuntu.sh
+++ scripts/docker/ubuntu.sh
@@ -1,10 +1,16 @@
 
 #!/bin/sh
 
-apt-get remove docker docker-engine docker.io docker-ce docker-ce-cli
+#ask for sudo promotion
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@" && echo -n "sudo bash what: "
+read WHAT
+sudo $WHAT
+
+apt-get remove docker docker-engine docker.io docker-ce docker-ce-cli containerd runc
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 usermod -aG docker c6supper
+gpasswd -a c6supper docker
 curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
@@ -13,8 +19,12 @@ rm -rf /etc/docker/daemon.json
 touch /etc/docker/daemon.json
 cat >> /etc/docker/daemon.json << EOF
 {
-    "registry-mirrors": ["https://registry.docker-cn.com"]
-} 
+  "registry-mirrors": [
+    "https://registry.docker-cn.com",
+    "http://hub-mirror.c.163.com",
+    "https://docker.mirrors.ustc.edu.cn"
+  ]
+}
 EOF
 
 service docker restart
