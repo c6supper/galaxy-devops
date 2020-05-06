0be99549bd09df19984123bd343cae1dda0ddabf
veex <c6supper@hotmail.com>
2020-05-06 18:04:10 +0800

    update

--- scripts/docker/ubuntu.sh
+++ scripts/docker/ubuntu.sh
@@ -1,14 +1,15 @@
 
 #!/bin/sh
 
+echo try install docker with $(logname)
 #ask for sudo promotion
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
 apt-get remove docker docker-engine docker.io docker-ce docker-ce-cli containerd runc
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-usermod -aG docker c6supper
-gpasswd -a c6supper docker
+usermod -aG docker $(logname)
+gpasswd -a $USER docker
 curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
