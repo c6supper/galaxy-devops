e68acee3bb7ba6fb1b37dd2be817bc7a3ea71724
c6supper <c6supper@hotmail.com>
2020-05-27 04:07:43 +0000

    add kafka

---
+++ scripts/kafka/README.md
@@ -0,0 +1,11 @@
+# kafka docker
+
+* kafka haven't offcial docker image, use a popular one
+    docker pull wurstmeister/kafka
+    
+* Tutorial
+    http://wurstmeister.github.io/kafka-docker/
+
+* Run docker
+    run example:
+    a. ./run.sh 
--- scripts/kafka/docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker pull bitnami/kafka:latest
-wget https://mirror.bit.edu.cn/apache/kafka/2.5.0/kafka_2.12-2.5.0.tgz
-# curl -sSL https://raw.githubusercontent.com/bitnami/bitnami-docker-kafka/master/docker-compose.yml > docker-compose.yml
