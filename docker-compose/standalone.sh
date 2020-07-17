a4aa4581fde3451c81b35f11171654a8808b9e13
c6supper <c6supper@hotmail.com>
2020-07-17 03:53:14 +0000

    fix the standalone start script

---
+++ docker-compose/standalone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+set -o allexport
+source "./.env"
+set +o allexport
+
+env | while read line; do
+  if [ ! -z "$line" ] ; then
+    if [[ $line == *"_VOLUME="* ]]; then
+      echo "$line" | awk 'BEGIN { FS = "=" } ; { system("mkdir -p \""$2"\"") }'
+    fi
+  fi
+done
+
