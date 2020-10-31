eb5b945c49fb017f82e0c6fca80429927be4e655
c6supper <c6supper@hotmail.com>
2020-10-31 02:07:20 +0000

    support rocketchat in openresty

--- docker-compose/standalone.sh
+++ docker-compose/standalone.sh
@@ -16,5 +16,5 @@ env | while read line; do
 done
 
 #docker-compose -f galaxy.yml restart
-cat galaxy.template.yaml | envsubst | docker-compose -f - -p "galaxy" up -d
-cat galaxy.template.yaml | envsubst > docker-compose.yml
+cat galaxy.template.yaml | DOLLAR='$' envsubst | docker-compose -f - -p "galaxy" up -d
+cat galaxy.template.yaml | DOLLAR='$' envsubst > docker-compose.yml
