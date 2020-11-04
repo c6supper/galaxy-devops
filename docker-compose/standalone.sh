a751677dbe147eb5aa9517316a82fec81edb7d37
c6supper <c6supper@hotmail.com>
2020-11-04 05:57:47 +0000

    enhance the script, support debug, add default environment variables for rocket.chat

--- docker-compose/standalone.sh
+++ docker-compose/standalone.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-docker stop `docker ps -aq -f name=galaxy_*`
-docker rm `docker ps -aq -f name=galaxy_*`
-docker network prune -f
-set -e
+#set -e
 set -o allexport
 source "./.env"
 set +o allexport
+
+__usage="
+Usage: $(basename $0) [OPTIONS]
+Options(optional only for debug purpose):
+  -APP_DEBUG_SERVER_URL ezremote.veexinc.net -APP_DEBUG_SERVER_HOST your_debug_server_ip
+"
+echo "$__usage"
 
 until [ $# -eq 0 ]
 do
@@ -21,6 +25,18 @@ env | while read line; do
   fi
 done
 
-#docker-compose -f galaxy.yml restart
-cat galaxy.template.yaml | DOLLAR='$' envsubst | docker-compose -f - -p "galaxy" up -d
+#generate the yaml file, and validate the configuration
 cat galaxy.template.yaml | DOLLAR='$' envsubst > docker-compose.yml
+docker-compose -f docker-compose.yml config > /dev/null
+if [ $? -eq 0 ]
+then
+    echo "docker-compose.yml passed validation."
+else
+    echo "Your docker-compose file got error, please check."
+fi
+
+#restart the services
+docker stop `docker ps -aq -f name=galaxy_*`
+docker rm `docker ps -aq -f name=galaxy_*`
+docker network prune -f
+docker-compose -p "galaxy" up -d
